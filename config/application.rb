e48ac8818634607adfba13fe9dd500781f9278bb
esquivel <esquivel@esquivels-MBP.T-mobile.com>
2018-04-07 11:47:20 -0700

    config asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.serve_static_assets = true
+    config.assets.enabled = true
 module Parranda
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
