18613095b90c2fd191c960d0fadae809313fbed4
esquivel <esquivel@esquivels-MBP.T-mobile.com>
2018-04-07 12:19:17 -0700

    update about route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'about/index'
   get 'kings_cup/index'
-
   get 'welcome/index'
-  root 'welcome#index'
   get 'power_hour/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+  
 end
