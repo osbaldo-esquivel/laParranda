35d1f30600ebb957f001e4eec1f2b049b7bc0e9f
esquivel <esquivel@esquivels-MBP.T-mobile.com>
2018-04-07 11:25:17 -0700

    update asset pipeline for heroku

--- app/assets/javascripts/count.js
+++ app/assets/javascripts/count.js
@@ -14,14 +14,18 @@ document.getElementById("drinkButt").addEventListener("click",function() {
             document.getElementById("counter").innerHTML = "Congrats on the Power Hour!";
         }
 
+        var cheers = document.getElementById("cheers");
+        console.log(cheers);
         if(secs == 0) {
             var audio = new Audio("/audios/bell.mp3");
             audio.play();
             var msg = document.getElementById("drinkFlash");
+            
             msg.innerText = "Drink!";
             setTimeout(function() {
                 msg.innerText = "";
             },3000);
+            
         }
     }, 1000);
 });
