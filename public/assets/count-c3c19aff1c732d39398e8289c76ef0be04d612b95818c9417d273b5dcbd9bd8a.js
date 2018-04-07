2d585a6a436a8d7fb94b061b12a5bfe9a276b318
esquivel <esquivel@esquivels-MBP.T-mobile.com>
2018-04-07 11:57:47 -0700

    add precompile config

---
+++ public/assets/count-c3c19aff1c732d39398e8289c76ef0be04d612b95818c9417d273b5dcbd9bd8a.js
@@ -0,0 +1,31 @@
+document.getElementById("drinkButt").addEventListener("click",function() {
+    var sixtyMinutes = new Date();
+    sixtyMinutes.setMinutes(sixtyMinutes.getMinutes() + 60);
+    var counterFunc = setInterval(function() {
+        var timeNow = new Date().getTime();
+        var diff = sixtyMinutes - timeNow;
+        var mins = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        var secs = Math.floor((diff % (1000 * 60)) / 1000);
+        if(secs < 10) { secs = "0" + secs; }
+        document.getElementById("counter").innerHTML = mins + ":" + secs;
+
+        if (diff < 0) {
+            clearInterval(counterFunc);
+            document.getElementById("counter").innerHTML = "Congrats on the Power Hour!";
+        }
+
+        var cheers = document.getElementById("cheers");
+        console.log(cheers);
+        if(secs == 0) {
+            var audio = new Audio("/audios/bell.mp3");
+            audio.play();
+            var msg = document.getElementById("drinkFlash");
+            
+            msg.innerText = "Drink!";
+            setTimeout(function() {
+                msg.innerText = "";
+            },3000);
+            
+        }
+    }, 1000);
+});
