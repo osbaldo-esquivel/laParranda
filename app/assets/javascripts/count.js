7f3c6af1dfa7ceaf378acccb094b4856dc0746b0
Osbaldo Esquivel <osbaldo.esquivel@gmail.com>
2018-04-17 20:53:27 -0700

    fix counter logic

--- app/assets/javascripts/count.js
+++ app/assets/javascripts/count.js
@@ -1,8 +1,9 @@
 var begin = true;
 var val = 0;
 
-function getVal(min) {
-    val = min;
+function getVal(mins) {
+    val = mins;
+    document.getElementById("counter").innerText = val + " mins";
 }
 
 document.getElementById("custom").addEventListener("click", function() {
@@ -10,30 +11,27 @@ document.getElementById("custom").addEventListener("click", function() {
 });
 
 document.getElementById("drinkButt").addEventListener("click",function() {
+    var currDate = new Date();
+    var timeEnds = currDate.setMinutes(currDate.getMinutes() + val*1000);
     console.log(val);
-    var minutes = new Date();
-    minutes.setMinutes(minutes.getMinutes() + val);
     var counterFunc = setInterval(function() {
+        var timeNow = Date.now();
+        var gameTime = timeEnds - timeNow;
+        var mins = Math.floor(gameTime/60000000);
+        var secs = Math.floor((gameTime/1000)%60);
+        var applause = new Audio("/audios/applause.mp3");
         var cheers = document.getElementById("cheers");
-        var timeNow = new Date().getTime();
-        var diff = minutes - timeNow;
-        var mins = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        var secs = Math.floor((diff % (1000 * 60)) / 1000);
-
         if(secs < 10) { secs = "0" + secs; }
-        document.getElementById("counter").innerHTML = mins + ":" + secs;
-        
-        if (diff < 0) {
+        if(mins < 10) { mins = "0" + mins; }
+        document.getElementById("counter").innerText = mins + ":" + secs;
+        if (mins == 0 && secs == 0) {
             clearInterval(counterFunc);
             document.getElementById("counter").innerHTML = "Congrats on the Power Hour!";
         }
-        
-        var applause = new Audio("/audios/applause.mp3");
         if(begin == true) {
             applause.play();
             begin = false;
         }
-
         if(secs == 0) {
             var audio = new Audio("/audios/bell.mp3");
             audio.play();
@@ -44,7 +42,7 @@ document.getElementById("drinkButt").addEventListener("click",function() {
                 msg.innerText = "";
                 cheers.style.display = "none";
             },3000);
-            
+        
         }
     }, 1000);
 });
