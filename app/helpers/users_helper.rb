909cf74ac09ea2081801239a9fb8bd6345d58536
Osbaldo Esquivel <osbaldo.esquivel@gmail.com>
2018-04-18 20:21:25 -0700

    create signup page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-    def gravatar_for(user)
+    def gravatar_for(user, size: 80)
         grav_id = Digest::MD5::hexdigest(user.email.downcase)
-        grav_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-        image_tag(grav_url, alt: user.name, class: "gravatar")
+        grav_url = "https://secure.gravatar.com/avatar/#{grav_id}?s=#{size}"
+        image_tag(grav_url, alt: user.name, class: "grav")
     end
 end
