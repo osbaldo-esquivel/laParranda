c3814e1e3fef2e7f23c51fd655f73ca7ef0b2269
Osbaldo Esquivel <osbaldo.esquivel@gmail.com>
2018-04-30 20:51:10 -0700

    add sign in functionality with dropdown links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    email_param = params[:session][:email].downcase
+    user = User.find_by(email: email_param)
+    if user && user.authenticate(params[:session][:password])
+      log_in user 
+      redirect_to user
+    else
+      flash.now[:danger] = 'Email and password are not correct'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    def curr_user
+        @curr_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def signed_in?
+      !curr_user.nil?
+    end  
 end
