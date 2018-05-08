2e41d1d201d565a0c46bbbd57a262ec43c936e76
Osbaldo Esquivel <osbaldo.esquivel@gmail.com>
2018-05-07 20:12:58 -0700

    remember digest migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { self.email = email.downcase }
-    validates :name, presence: true, length: { maximum: 20 }
-    EMAIL_VALID_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates :name,  presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
-              uniqueness: { case_sensitive: false },
-              format: { with: EMAIL_VALID_REGEX }
-    validates :password, presence: true, length: { minimum: 6 }
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+  
+    def self.digest(str)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(str, cost: cost)
+    end
+  
+    def self.new_token
+      SecureRandom.urlsafe_base64
+    end
+  
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
 end
